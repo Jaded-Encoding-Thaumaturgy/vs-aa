e8e1e102da794c8f144a9755767f78102d2d848a
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-26 01:14:01 +0200

    Add __init__

---
+++ vsaa/__init__.py
@@ -0,0 +1,2 @@
+from .abstract import *  # noqa: F401, F403
+from .antialiasers import *  # noqa: F401, F403
---
+++ vsaa/antialiasers/__init__.py
@@ -0,0 +1,5 @@
+from .eedi2 import *  # noqa: F401, F403
+from .eedi3 import *  # noqa: F401, F403
+from .nnedi3 import *  # noqa: F401, F403
+from .sangnom import *  # noqa: F401, F403
+from .znedi3 import *  # noqa: F401, F403
