35cbe8b7edfd98c23d1884dc0b9ace09bd458303
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-26 17:00:01 +0200

    Add clamp_aa

--- vsaa/funcs.py
+++ vsaa/funcs.py
@@ -1,9 +1,11 @@
 import vapoursynth as vs
+from vsexprtools.util import PlanesT, norm_expr_planes, normalise_planes
 from vskernels import Catrom, Kernel
-from vsutil import get_w, join, split
+from vsutil import get_peak_value, get_w, join, split
 
 from .abstract import SingleRater, SuperSampler
 from .antialiasers import Eedi3SR, Nnedi3SS
+from vsrgtools import median_clips
 from .enums import AADirection
 
 core = vs.core
@@ -82,3 +84,40 @@ def taa(clip: vs.VideoNode, aafunc: SingleRater) -> vs.VideoNode:
         return aa_y
 
     return join([aa_y, *chroma], clip.format.color_family)
+
+
+def clamp_aa(
+    src: vs.VideoNode, weak: vs.VideoNode, strong: vs.VideoNode,
+    strength: float = 1, planes: PlanesT = 0
+) -> vs.VideoNode:
+    """
+    Clamp stronger AAs to weaker AAs.
+    Useful for clamping :py:func:`upscaled_sraa` or :py:func:`Eedi3SR` to :py:func:`Nnedi3SR`
+    for a strong but more precise AA.
+
+    Original function written by `Zastin <https://github.com/kgrabs>`_,
+    modified by LightArrowsEXE, Setsugen no ao.
+
+    :param src:         Non-AA'd source clip.
+    :param weak:        Weakly-AA'd clip (eg: :py:func:`lvsfunc.aa.nnedi3`).
+    :param strong:      Strongly-AA'd clip (eg: :py:func:`lvsfunc.aa.eedi3`).
+    :param strength:    Clamping strength (Default: 1).
+
+    :return:            Clip with clamped anti-aliasing.
+    """
+    assert src.format
+
+    planes = normalise_planes(src, planes)
+
+    if src.format.sample_type == vs.INTEGER:
+        thr = strength * get_peak_value(src)
+    else:
+        thr = strength / 219
+
+    if thr == 0:
+        return median_clips([src, weak, strong], planes)
+
+    expr = f'x y - XYD! XYD@ x z - XZD! XZD@ xor x XYD@ abs XZD@ abs < z y {thr} + min y {thr} - max z ? ?'
+
+    return core.akarin.Expr([src, weak, strong], norm_expr_planes(src, expr, planes))
+
