dbd07ebcf731c2cd6916969ab4320e62f94daf21
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-22 17:12:19 +0100

    Bump major version

--- vsaa/_metadata.py
+++ vsaa/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth anti aliasing and scaling functions"""
 
-__version__ = '1.4.3'
+__version__ = '1.5.0'
 
 __author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
 __maintainer__ = __author__
