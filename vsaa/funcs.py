723713a6e1b2d66049e6ef4a19f19b71d3b14f1c
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-09-23 07:49:25 +0200

    based_aa: Use modulo, less ugly if statement

--- vsaa/funcs.py
+++ vsaa/funcs.py
@@ -397,11 +397,10 @@ else:
         aaw, aah = [(round(r * rfactor) + 1) & ~1 for r in (ss_clip.width, ss_clip.height)]
 
         if downscaler is None:
-            if ((max(aaw, func.work_clip.width) / min(aaw, func.work_clip.width)).is_integer()
-                    and (max(aah, func.work_clip.height) / min(aah, func.work_clip.height)).is_integer()):
-                downscaler = Box
-            else:
-                downscaler = Catrom
+            downscaler = Box if (
+                max(aaw, func.work_clip.width) % min(aaw, func.work_clip.width) == 0
+                and max(aah, func.work_clip.height) % min(aah, func.work_clip.height) == 0
+            ) else Catrom
 
         if rfactor < 1.0:
             downscaler, supersampler = supersampler, downscaler
